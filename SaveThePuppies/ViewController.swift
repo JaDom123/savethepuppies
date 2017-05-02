757c7586c1fd97bea730af687b6002fe1b7da3a1
Jason Swift <jdswift94@gmail.com>
2017-05-02 10:56:48 +0100

    Initial Commit

---
+++ SaveThePuppies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveThePuppies
+//
+//  Created by Jason Swift on 02/05/2017.
+//  Copyright © 2017 JasonSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
